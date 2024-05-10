9e4129bc617f3449fadcdabd2349e87f85390263
AnnaEmkha <152389555+AnnaEmkha@users.noreply.github.com>
2024-05-10 22:12:56 +0300

    step-3

---
+++ src/js/2-form.js
@@ -0,0 +1,33 @@
+const formData = { email: '', message: '' };
+const form = document.querySelector(`.feedback-form`);
+form.addEventListener('input', handleInput);
+form.addEventListener('submit', sendData);
+
+function handleInput(event) {
+  const key = event.target.name;
+  formData[key] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function fillText() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (!data) {
+    return;
+  }
+  const { email, message } = form.elements;
+  email.value = data.email;
+  message.value = data.message;
+}
+
+function sendData(event) {
+  const { email, message } = form.elements;
+  event.preventDefault();
+  if (email.value === '' || message.value === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  localStorage.removeItem('feedback-form-state');
+  formData.email = '';
+    formData.message = '';
+    form.reset();
+}
